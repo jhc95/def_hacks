a05906c6d8ac86350a54878eb816dc28c1c0647c
jhc95 <narsineke@gmail.com>
2019-04-14 05:58:11 -0700

    'yay'

--- extension/log.js
+++ extension/log.js
@@ -34,6 +34,9 @@ function amount() {
 		amount = document.getElementById("subtotal").amount.value;
 		document.getElementById("loanform").style.display = "block";
 		document.getElementById("subtotal").style.display = "none";
+		document.getElementById("credit-offer").style.display = "none";
+		document.getElementById("load1").innerHTML = "Loading...";
+		document.getElementById("load2").innerHTML = "Loading...";
 		chrome.storage.local.set({'items': amount});
 		chrome.storage.local.get(['items','password','userid'], function(item) {
 			if (item.amount != 0) {
@@ -61,15 +64,42 @@ function amount() {
 						appendChildElement3 = newli.appendChild(newP2);
 						appendChildElement3.innerHTML = "Recommendation Score: " + data.loanOffers[i].recommendationScore;
 						appendChildElement1 = newli.appendChild(newA);
-						appendChildElement1.innerHTML = data.loanOffers[i].url;
+						appendChildElement1.innerHTML = "More Details";
 						appendChildElement4 = par.appendChild(newli);
+						document.getElementById("load1").innerHTML = "Offers";
 					}
-					
+					for (i = 0; i < data.creditCardOffers.length; i++) {
+						var par = document.getElementById("div2");
+						var newli = document.createElement('LI');
+						var newA = document.createElement('A');
+						var newP = document.createElement('P');
+						var newP2 = document.createElement('P');
+						newA.href = data.creditCardOffers[i].url
+						newA.target = "_blank"
+						appendChildElement2 = newli.appendChild(newP);
+						appendChildElement2.innerHTML = "Card Type: " + data.creditCardOffers[i].details.cardType;
+						appendChildElement3 = newli.appendChild(newP2);
+						appendChildElement3.innerHTML = "Recommendation Score: " + data.creditCardOffers[i].recommendationScore;
+						appendChildElement1 = newli.appendChild(newA);
+						appendChildElement1.innerHTML = "More Details";
+						appendChildElement4 = par.appendChild(newli);
+						document.getElementById("load2").innerHTML = "Offers";
+					}
 				})
 			} 
 		});
 	}
 }
+function openOffer1() {
+	document.getElementById("loan-offer").style.display = "block";
+	document.getElementById("credit-offer").style.display = "none";
+}
+
+function openOffer2() {
+	document.getElementById("loan-offer").style.display = "none";
+	document.getElementById("credit-offer").style.display = "block";
+}
+
 
 document.getElementById("log_button").addEventListener("click", function() {check()});
 document.getElementById("total").addEventListener("click", function() {amount()});
@@ -81,4 +111,7 @@ chrome.storage.local.get(['logged'], function(item) {
 		document.getElementById("subtotal").style.display = "block";
 	}
 });
+
+document.getElementById("loans").addEventListener("click", function() {openOffer1()})
+document.getElementById("credits").addEventListener("click", function() {openOffer2()})
 
